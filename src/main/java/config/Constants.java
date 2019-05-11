ffed7f6c19d54c93c469dd92020c1bcabd69883f
Justin Harwood <justin@harwoodjp.com>
2019-05-11 17:42:03 -0500

    better default path to /plot.html

--- src/main/java/config/Constants.java
+++ src/main/java/config/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
 	public static final String VALUE = "value";
 	public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
-	public static final String VISUALIZER_OUTPUT_PATH = "./plot.html";
+	public static final String VISUALIZER_OUTPUT_PATH = "./src/main/java/plot.html";
 	public static final String T = "T";
 
 	public enum Interval {SECOND, MINUTE, HOUR }
